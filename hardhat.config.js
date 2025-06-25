7e2ebb06c22faa4d6136d43ea6e586b2ab6032ba
Kay Chan <cwl196@ho.ha.org.hk>
2025-06-25 17:20:05 +0800

    chore: update timeout

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,7 +42,9 @@ module.exports = {
         '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a',
         '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba',
         '0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e',
-      ]
+      ],
+      blockGasLimit: 30000000,
+      timeout: 600000, // 10 minutes (600,000 ms)
     },
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
