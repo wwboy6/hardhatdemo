fa1d89af79298e2878ea781152271975bb6c2889
Kay Chan <cwl196@ho.ha.org.hk>
2025-06-26 16:48:01 +0800

    chore: update include file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,18 @@
 const hre = require("hardhat");
-const { ChainId } = require('@pancakeswap/sdk')
+// const { ChainId } = require('@pancakeswap/sdk')
+const { bscTokens } = require('@pancakeswap/tokens')
 const { SMART_ROUTER_ADDRESSES } = require('@pancakeswap/smart-router')
+const { CommandType, pancakeswapUniversalRouter, uniswapUniversalRouter } = require('../include/universal-router')
+const { loanPoolProvider } = require('../include/aave')
 
-const chainId = ChainId.BSC
-const smartRouterAddress = SMART_ROUTER_ADDRESSES[chainId];
+// const chainId = ChainId.BSC
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contracts with account:", deployer.address);
+
+  const balance0 = await hre.ethers.provider.getBalance(deployer.address)
+  console.log(`balance0 ${balance0}`)
 
   // const SimpleTest = await hre.ethers.getContractFactory("SimpleTest");
   // const test = await SimpleTest.deploy()
@@ -15,17 +20,24 @@ async function main() {
   // console.log("SimpleTest deployed to:", await test.getAddress());
 
   // const FlashLoadSmartRouter = await hre.ethers.getContractFactory("FlashLoadSmartRouter");
-  // const router = await FlashLoadSmartRouter.deploy(smartRouterAddress)
+  // const router = await FlashLoadSmartRouter.deploy(pancakeswapUniversalRouter)
   // await router.waitForDeployment();
   // console.log("FlashLoadSmartRouter deployed to:", await router.getAddress());
 
-  const balance0 = await hre.ethers.provider.getBalance(deployer.address)
-  console.log(`balance0 ${balance0}`)
+  // const PancakeswapArbitrage = await hre.ethers.getContractFactory("PancakeswapArbitrage");
+  // const router = await PancakeswapArbitrage.deploy(pancakeswapUniversalRouter)
+  // await router.waitForDeployment();
+  // console.log("PancakeswapArbitrage deployed to:", await router.getAddress());
 
-  const PancakeswapArbitrage = await hre.ethers.getContractFactory("PancakeswapArbitrage");
-  const router = await PancakeswapArbitrage.deploy(smartRouterAddress)
+  const UniversalArbitrage = await hre.ethers.getContractFactory("UniversalArbitrage");
+  const router = await UniversalArbitrage.deploy(
+    pancakeswapUniversalRouter,
+    uniswapUniversalRouter,
+    loanPoolProvider,
+    bscTokens.wbnb.address,
+  )
   await router.waitForDeployment();
-  console.log("PancakeswapArbitrage deployed to:", await router.getAddress());
+  console.log("UniversalArbitrage deployed to:", await router.getAddress());
 
   const balance1 = await hre.ethers.provider.getBalance(deployer.address)
   const currencyInHkd = 5062
